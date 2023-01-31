ff6e9fe4e61cb4e447fe68559e9f5e807b07a974
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-01-31 16:01:54 +0000

    Add arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,46 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+// 1. Vanilla JavaScript Function
 
-// Arrow Function With Parameters
+//function addTwoNumbers(a, b) {
+// Code block
+//    return a + b;
+//}
+//let sum = addTwoNumbers(3, 5);
+//console.log(sum);
 
-// Single Line Arrow Function With Parameters
+// terminal: node arrow-functions.js
 
-// Implicit Returns
+// 2. Arrow Function With Parameters
 
-// Returning Multiple Lines
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
+// 3. Single Line Arrow Function With Parameters
+
+// const addTwoNumbers2 = (a, b) => (a + b);    also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2)
+
+// 4. Implicit Returns
+
+const saySomething = message => console.log(message);
+saySomething('Hello, World!');
+
+const sayHello = () => console.log('hello hi bonjour');
+sayHello();
+
+// 5. Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
